4cf3501c1ac75d5095d78dc1dd97393a01c8ffeb
kazaminn <90266173+kazaminn@users.noreply.github.com>
2026-07-01 11:46:42 +0900

    🧹 chore: configファイルの変更
    - next.config.js -> TSファイル
    - postcss.config.js -> CommonJS拡張子に変更

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  distDir: "dist",
+  images: {
+    loader: "custom",
+    imageSizes: [],
+    // コンテンツ用 (モバイルRetina(640), Tailwind lg(1024), 最大(1920))
+    deviceSizes: [640, 1024, 1920],
+  },
+  transpilePackages: ["next-image-export-optimizer"],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "dist",
+    nextImageExportOptimizer_exportFolderName: "optimized",
+    nextImageExportOptimizer_quality: "75",
+    nextImageExportOptimizer_storePicturesInWEBP: "true",
+    nextImageExportOptimizer_generateAndUseBlurImages: "false",
+    nextImageExportOptimizer_remoteImageCacheTTL: "0",
+  },
+};
+
+export default nextConfig;
